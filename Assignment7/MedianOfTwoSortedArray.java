0392c8d6cfba03851e9006ffcafa11d43c025259
allenyzs <yzsallen@gmail.com>
2018-11-12 15:18:04 -0800

    update

--- Assignment7/MedianOfTwoSortedArray.java
+++ Assignment7/MedianOfTwoSortedArray.java
@@ -3,35 +3,41 @@ package assignment7;
 public class MedianOfTwoSortedArray {
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length){
-            findMedianSortedArrays(nums2, nums1);
+            int[] temp = nums1; nums1 = nums2; nums2 = temp;
         }
-        int len = nums1.length + nums2.length;
-        int cut1 = 0;
-        int cut2 = 0;
-        int cutL = 0;
-        int cutR = nums1.length;
-
-        while (cut1 <= nums1.length){
-            cut1 = cutL + (cutR - cutL) / 2;
-            cut2 = len / 2 - cut1;
-            double L1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            double L2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            double R1 = (cut1 == 0) ? Integer.MAX_VALUE : nums1[cut1];
-            double R2 = (cut2 == 0) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if (L1 > R2){
-                cutR = cut1 - 1;
-            } else if (L2 > R1){
-                cutL = cut1 + 1;
-            } else {
-                if (len % 2 == 0){
-                    L1 = L1 > L2 ? L1 : L2;
-                    R1 = R1 < R2 ? R1 : R2;
-                    return (L1 + R1) / 2;
+        int halfLen = (nums1.length + nums2.length + 1) / 2;
+        int min = 0, max = nums1.length;
+        // 如果我们知道nums1怎么切 我们就知道nums2该怎么切
+        while (min <= max){
+            int i = (min + max) / 2;
+            int j = halfLen - i;
+            
+            // L1 L2 R1 R2需要满足以下关系 ：
+            // L1 < R2 && L2 < R1
+            if (i < max && nums2[j - 1] > nums1[i]){
+                min = i + 1;
+            } else if (i > min && nums1[i - 1] > nums2[j]){
+                max = i - 1;
+            } else{
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0){
+                    maxLeft = nums1[i - 1];
                 } else{
-                    R1 = R1 < R2 ? R1 : R2;
-                    return R1;
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
                 }
+                if ((nums1.length + nums2.length) % 2 == 1) return maxLeft;
+                
+                int minRight = 0;
+                if (i == nums1.length) {
+                    minRight = nums2[j];
+                } else if (j == nums2.length) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+                return (maxLeft + minRight) / 2.0;
             }
         }
         return (double) -1;
